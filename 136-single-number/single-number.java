b5b71a85f71e85525ef9ba26b9475688ada23222
SuryaS03 <126948915+SuryaS03@users.noreply.github.com>
2024-06-26 11:29:03 +0530

    Time: 7 ms (29.72%) | Memory: 46.5 MB (7.50%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int ans=0;
+        for(int i:nums){
+            ans=ans^i;
+        }
+        
+        
+        
+   
+   return ans; }
+}
