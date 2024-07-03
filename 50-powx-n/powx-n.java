de5b17c82f9202385d7a3e5736d8e260c0af7876
SuryaS03 <126948915+SuryaS03@users.noreply.github.com>
2024-07-03 11:16:20 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (5.51%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double myPow(double x, int n) {
+        // int z=1;
+        // if(n==0)
+        // return 1;
+        // else
+        // return 2;
+
+        // return z*myPow(x,n-1);
+        return Math.pow(x,n);
+        
+    }
+}
