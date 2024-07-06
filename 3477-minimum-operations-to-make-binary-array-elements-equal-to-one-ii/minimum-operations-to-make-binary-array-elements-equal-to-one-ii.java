5b45891bbb72dc1c1fb32e0701fa0969efbbf075
SuryaS03 <126948915+SuryaS03@users.noreply.github.com>
2024-07-06 16:46:11 +0530

    Time: 7 ms (61.00%) | Memory: 56.8 MB (65.50%) - LeetSync

--- 3477-minimum-operations-to-make-binary-array-elements-equal-to-one-ii/minimum-operations-to-make-binary-array-elements-equal-to-one-ii.java
+++ 3477-minimum-operations-to-make-binary-array-elements-equal-to-one-ii/minimum-operations-to-make-binary-array-elements-equal-to-one-ii.java
@@ -2,13 +2,20 @@ class Solution {
     public int minOperations(int[] nums) {
         int count=0;
         int flip=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==flip){
-count++;
-flip=1-flip;
-            }
-        }
-        return count;
+       for(int i=0;i<nums.length;i++){
+
+if(nums[i]==0 && flip%2==0){
+    count++;
+    flip++;
+}
+if(nums[i]==1 && flip %2==1)
+{
+    count++;
+    flip++;
+}
+
+       }
+       return count;
 
     }
 }
