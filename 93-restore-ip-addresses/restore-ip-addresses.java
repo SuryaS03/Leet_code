a03a059cae3f14abe4828354573883588429bd32
SuryaS03 <126948915+SuryaS03@users.noreply.github.com>
2024-10-08 17:10:14 +0530

    Time: 12 ms (6.88%) | Memory: 45.4 MB (5.73%) - LeetSync

---
+++ 93-restore-ip-addresses/restore-ip-addresses.java
@@ -0,0 +1,36 @@
+class Solution {
+
+public static boolean isValid(String s){
+    if(s.length()>3)
+    return false;
+    if(s.length()>1 && s.charAt(0)=='0')
+    return false;
+    int a=Integer.parseInt(s);
+    if(a<0 || a>255)
+    return false;
+    return true;
+
+}
+
+    public static void solve(String s,int idx,int part,List<String> res,String cur){
+        if(idx==s.length()){
+            if(part==4)
+            res.add(cur.substring(0,cur.length()-1));
+            return;
+        }
+        for(int i=idx;i<s.length();i++){
+            if(isValid(s.substring(idx,i+1))){
+solve(s,i+1,part+1,res,cur+s.substring(idx,i+1)+".");
+            }
+        }
+    }
+
+
+
+    public List<String> restoreIpAddresses(String s) {
+        List <String> res=new ArrayList<>();
+        solve(s,0,0,res,"");
+        return res;
+        
+    }
+}
