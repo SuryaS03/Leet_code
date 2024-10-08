8e636da9c9cd862f6f91b91911d42b4aa494acae
SuryaS03 <126948915+SuryaS03@users.noreply.github.com>
2024-10-08 22:27:19 +0530

    Time: 12 ms (6.88%) | Memory: 45.5 MB (5.73%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.java
+++ 93-restore-ip-addresses/restore-ip-addresses.java
@@ -1,4 +1,16 @@
 class Solution {
+public static void solve(String s,int idx,int part,List<String> res,String cur){
+    if(idx==s.length()){
+        if(part==4)
+        res.add(cur.substring(0,cur.length()-1));
+        return;
+    }
+    for(int i=idx;i<s.length();i++){
+        if(isValid(s.substring(idx,i+1))){
+            solve(s,i+1,part+1,res,cur+s.substring(idx,i+1)+".");
+        }
+    }
+}
 
 public static boolean isValid(String s){
     if(s.length()>3)
@@ -9,21 +21,8 @@ public static boolean isValid(String s){
     if(a<0 || a>255)
     return false;
     return true;
-
 }
 
-    public static void solve(String s,int idx,int part,List<String> res,String cur){
-        if(idx==s.length()){
-            if(part==4)
-            res.add(cur.substring(0,cur.length()-1));
-            return;
-        }
-        for(int i=idx;i<s.length();i++){
-            if(isValid(s.substring(idx,i+1))){
-solve(s,i+1,part+1,res,cur+s.substring(idx,i+1)+".");
-            }
-        }
-    }
 
 
 
@@ -31,6 +30,5 @@ solve(s,i+1,part+1,res,cur+s.substring(idx,i+1)+".");
         List <String> res=new ArrayList<>();
         solve(s,0,0,res,"");
         return res;
-        
     }
 }
