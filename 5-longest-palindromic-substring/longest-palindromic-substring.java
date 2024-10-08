fb76743dd6525d73002bb34acbda879274e16217
SuryaS03 <126948915+SuryaS03@users.noreply.github.com>
2024-10-08 22:16:40 +0530

    Time: 19 ms (65.37%) | Memory: 45 MB (49.33%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,29 +1,27 @@
 class Solution {
-
-        public static String fun(String s,int l,int r){
-            while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r))
-          {  l--;
-            r++;}
-        
-        return s.substring(l+1,r);}
-
-
+ public static String fun(int l,int r,String s){
+    while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r))
+    {
+        l--;
+        r++;
+    }
+    return s.substring(l+1,r);
+ }
 
 
     public String longestPalindrome(String s) {
-        
+        String res=s.substring(0,1);
         String odd="";
         String even="";
-        String max=s.substring(0,1);
-        for(int i=0;i<s.length();i++){
-            odd=fun(s,i,i);
-            even=fun(s,i,i+1);
-            if(odd.length()>max.length())
-            max=odd;
-           if(even.length()>max.length())
-           max=even;
 
+        for(int i=0;i<s.length();i++){
+odd=fun(i,i,s);
+even=fun(i,i+1,s);
+if(odd.length()>res.length())
+res=odd;
+if(even.length()>res.length())
+res=even;
         }
-        return max;
+        return res;
     }
 }
