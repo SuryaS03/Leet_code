15a0d5795d96918c2e762e033592a71c2777e469
SuryaS03 <126948915+SuryaS03@users.noreply.github.com>
2024-10-08 21:43:45 +0530

    Time: 10 ms (22.30%) | Memory: 55.9 MB (95.02%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,41 +1,39 @@
 class Solution {
-    static boolean ispalin(String s)
-    {
-        int i=0;
-        int j=s.length()-1;
-        while(i<=j)
-        {
-            if(s.charAt(i)!=s.charAt(j))
-            {
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
+
+public static boolean isPali(String s){
+    int i=0;
+    int j=s.length()-1;
+    while(i<j){
+        if(s.charAt(i)!=s.charAt(j))
+        return false;
+        i++;
+        j--;
     }
-    static void part(String s,int idx,List<String> cur,List<List<String>> ans)
-    {
-        if(idx==s.length())
-        {
-            ans.add(new ArrayList<>(cur));
-            return;
+    return true;
+}
+
+
+    public static void part(String s,int idx,List<List<String>> res,List<String> cur){
+        if(idx==s.length()){
+            res.add(new ArrayList(cur));
+            return ;
         }
-        for(int i=idx;i<s.length();i++)
-        {
-            if(ispalin(s.substring(idx,i+1)))
-            {
+        for(int i=idx;i<s.length();i++){
+            if(isPali(s.substring(idx,i+1))){
                 cur.add(s.substring(idx,i+1));
-                part(s,i+1,cur,ans);
+                part(s,i+1,res,cur);
                 cur.remove(cur.size()-1);
             }
         }
     }
+
+
+
     public List<List<String>> partition(String s) {
-        List<List<String>> ans=new ArrayList<>();
-        List<String> cur=new ArrayList<>();
-        part(s,0,cur,ans);
-        return ans;
-        
+        List<List<String>> res=new ArrayList<>();
+        List<String> cur =new ArrayList<>();
+        part(s,0,res,cur);
+        return res;
+
     }
 }
