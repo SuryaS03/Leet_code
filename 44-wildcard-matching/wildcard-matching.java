e4f71ef8cd48a00e043162eb97ddd4040c722cf7
SuryaS03 <126948915+SuryaS03@users.noreply.github.com>
2025-01-31 19:10:48 +0530

    Time: 22 ms (41.87%) | Memory: 44.9 MB (53.63%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -10,11 +10,10 @@ class Solution {
         }
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
-                if(s.charAt(i-1)==p.charAt(j-1) ||p.charAt(j-1)=='?')
+                if(s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1)=='?')
                 dp[i][j]=dp[i-1][j-1];
-              else  if(p.charAt(j-1)=='*')
+                else if(p.charAt(j-1)=='*')
                 dp[i][j]=dp[i-1][j]||dp[i][j-1];
-
             }
         }
         return dp[m][n];
